c8f95e4a14894e0079952560a8513d2d1e526a2d
angelo-volpe <angelovolpe95@gmail.com>
2025-01-20 09:45:29 +0100

    job for model training

--- airflow/dags/document_ocr_training.py
+++ airflow/dags/document_ocr_training.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import timedelta
+from docker.types import Mount
 
 
 default_args = {
@@ -29,20 +30,21 @@ with DAG(
         auto_remove=True,
         docker_url="unix://var/run/docker.sock",
         network_mode="host",
+        mounts=[Mount(source="/Users/volpea/Documents/projects/document-generator-job/data", target="/app/data", type="bind")],
         mount_tmp_dir=False,
         command="--job_name sampling" + " {{ '--document_id ' + dag_run.conf['document_id'] }}",
     )
 
-    #training = DockerOperator(
-    #    task_id="train_model",
-    #    image="paddle_ocr:latest",
-    #    api_version="auto",
-    #    auto_remove=True,
-    #    docker_url="unix://var/run/docker.sock",
-    #    network_mode="host",
-    #    mounts=[Mount(source="/Users/volpea/Documents/projects/document-generator-job/data", target="/app/data", type="bind")],
-    #    mount_tmp_dir=False,
-    #    command="",
-    #)
-
-    #sample_preprocessing >> training
+    training = DockerOperator(
+        task_id="train_model",
+        image="paddle_ocr:latest",
+        api_version="auto",
+        auto_remove=True,
+        docker_url="unix://var/run/docker.sock",
+        network_mode="host",
+        shm_size=2*1024*1024*1024,
+        mounts=[Mount(source="/Users/volpea/Documents/projects/document-generator-job/data", target="/data", type="bind"),
+                Mount(source="/Users/volpea/Documents/projects/PaddleOCR/models", target="/models", type="bind")],
+        mount_tmp_dir=False,
+        command="./document_app/train_new_model.sh /data/fine_tuning_dataset {{ dag_run.conf['document_id'] }}",
+    )
